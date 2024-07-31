d335b11fd3df75efec4193d96eece26ec9fc8120
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-07-31 12:11:58 -0500

    feat: :sparkles: API 2.2

---
+++ controllers/funcionController.js
@@ -0,0 +1,82 @@
+import { connect } from '../config/connect.js'
+import { ObjectId } from 'mongodb';
+
+const connection = new connect();
+
+export class funcionController {
+    /**
+     * * NOTA: Clase para metodos respecto a peliculas
+     */
+    constructor(){
+        this.connection = connection;
+        this.collection = this.connection.getCollection('funcion');
+    }
+    /**
+     * * NOTA:Mostrar funciones disponibles
+     * @returns Funciones disponibles
+     */
+    async apiUno (){
+        try {
+            const cursor = await this.collection.aggregate(
+            [
+                {
+                $lookup: {
+                    from: 'pelicula',
+                    localField: 'id_pelicula',
+                    foreignField: '_id',
+                    as: 'pelicula_info'
+                }
+                },
+                { $unwind: '$pelicula_info' },
+                {
+                $lookup: {
+                    from: 'sala',
+                    localField: 'id_sala',
+                    foreignField: '_id',
+                    as: 'sala_info'
+                }
+                },
+                { $unwind: '$sala_info' },
+                {
+                $project: {
+                    pelicula: '$pelicula_info.titulo',
+                    fecha_init: 1,
+                    sala: '$sala_info.nombre',
+                    fecha_end: 1
+                }
+                }
+            ],
+            { maxTimeMS: 60000, allowDiskUse: true }
+            );
+              // * Get the cursor
+            const result = await cursor.toArray(); // * Convert the cursor to an array
+            console.table(result); // * Print the results
+            await this.connection.close(); // * Close the connection
+            return result; // * Return the results
+        } 
+        catch (error) {
+            // ! Handle errors
+            console.error("Error fetching data or closing connection:", error);
+        }
+    }
+    /**
+     * 
+     * @param {String} idFuncion Id de la funcion a consultar, ej:66a807cca5aad36c22a20ca3 
+     * @returns asientos disponibles de la funcion
+     */
+    async apiDos (idFuncion){
+        try {
+            const cursor = await this.collection.findOne(
+                {"_id": new ObjectId(idFuncion)},
+                {projection: {"_id":0, "asientos": {"$elemMatch": {"estado": "disponible"}}}}
+            ); // * Get the cursor
+            console.table(cursor); // * Print the results
+            await this.connection.close(); // * Close the connection
+            return cursor; // * Return the results
+        } 
+        catch (error) {
+            // ! Handle errors
+            console.error("Error fetching data or closing connection:", error);
+        }
+    }
+}
