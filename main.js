fecf820f7c14d036e7ea5290b6f7db3918a4b918
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-08-01 09:57:13 -0500

    feat: :sparkles: import modules

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import {connect} from "./config/connect.js";
 import { funcionController } from "./controllers/funcionController.js";
 import { boletoController } from "./controllers/boletosController.js";
 import { ObjectId } from 'mongodb';
+import { usuarioController } from "./controllers/usersController.js";
 
