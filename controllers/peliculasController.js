08a4ee5d915a0d67ab07aa20c94ed49941eb412d
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-08-01 10:49:44 -0500

    fix: :bug: add close connection

--- controllers/peliculasController.js
+++ controllers/peliculasController.js
@@ -23,7 +23,27 @@ export class peliculaController {
                 { projection: { "titulo": 1, "genero": 1, "duracion": 1,  "_id": 0 } }); // * Get the cursor
             const result = await cursor.toArray(); // * Convert the cursor to an array
             console.table(result); // * Print the results
+            await this.connection.close(); // * Close the connection
             return result; // * Return the results
+        } 
+        catch (error) {
+            // ! Handle errors
+            console.error("Error fetching data or closing connection:", error);
+        }
+    }
+    /**
+     * 
+     * @param {String} idPelicula Id de la pelicula a consultar, ej:66a80379a5aad36c22a20c80 
+     * @returns Informacion de la pelicula
+     */
+    async apiDos (idPelicula){
+        try {
+            const cursor = await this.collection.findOne(
+                { "_id": new ObjectId(idPelicula) }
+            ); // * Get the cursor
+            console.log(cursor); // * Print the results
+            await this.connection.close(); // * Close the connection
+            return cursor; // * Return the results
         } 
         catch (error) {
             // ! Handle errors
